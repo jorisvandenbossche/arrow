7e3bbd2c20610213f0ab8f3fbfa35e28c887dd4b
Benjamin Kietzman <bengilgit@gmail.com>
2019-11-27 14:41:14 -0500

    add PartitionSchemeDiscovery for inferring partition schemas

--- cpp/src/arrow/type.h
+++ cpp/src/arrow/type.h
@@ -1380,6 +1380,8 @@ class ARROW_EXPORT Schema : public detail::Fingerprintable,
   std::string ComputeMetadataFingerprint() const override;
 
  private:
+  friend void PrintTo(const Schema& s, std::ostream* os);
+
   class Impl;
   std::unique_ptr<Impl> impl_;
 };
